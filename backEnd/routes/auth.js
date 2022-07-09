827529cf5f581a387db1cd1cae94666bd8c614e4
sanchit arora <sanchit_arora@sanchits-MacBook-Air.local>
2022-07-09 14:25:56 +0530

    JWT added

--- backEnd/routes/auth.js
+++ backEnd/routes/auth.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../models/UserSchema');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
+const secretKey = "ThisKeyIsUsedForJwtTokenForTestingWebsite"
 
 //  console.log("check1")
-
-
-
-
-
 router.post('/signup',
   [body('name', 'Name should be greater than 3').isLength({ min: 3 }),
   body('email', 'Please enter a valid email').isEmail(),
@@ -25,15 +23,28 @@ router.post('/signup',
       // console.log(incomingUser)
       if (incomingUser)
           return res.status(400).json({ "errors": "Email already registered"});
-
+      
+      const salt = await bcrypt.genSalt(10);
+      const secPassword =  await bcrypt.hash(req.body.password,salt)
           // console.log("check4")
        incomingUser = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: secPassword
       })
+      
+      //create data for jwt. Always add the the data which will be unique in DB.
+      const data = {
+        user:{
+          name: req.body.name,
+          id: req.body.email,
+        }
+      }
+
+      const authtoken = jwt.sign(data,secretKey);
+
       res.json({
-        email: req.body.email,
+        authtoken: authtoken,
         message: "Registered Successfully"
       });
     }
