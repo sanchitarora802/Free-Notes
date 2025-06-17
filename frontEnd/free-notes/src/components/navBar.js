20e2c221e9cf61865185c8a37d55c5520074504a
sanchitarora802 <sanchit.arora802@gmail.com>
2025-06-17 17:38:28 +0530

    login button visible responsive

--- frontEnd/free-notes/src/components/navBar.js
+++ frontEnd/free-notes/src/components/navBar.js
@@ -240,79 +240,63 @@ function NavBar(props) {
         <Link className={`navbar-brand text-${props.textcolor}`} to="/">
           Free-Notes
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li
-              className={`nav-item ${
-                location.pathname === "/addnotes" ? "active" : ""
-              } `}
-            >
-              <Link
-                className={` nav-link text-${props.textcolor}`}
-                to="/addNotes"
-              >
-                AddNotes{" "}
-              </Link>
-            </li>
-          </ul>
-
-          <div
-            className={`form-check form-switch text-${
-              props.mode === `light` ? `dark` : `light`
-            }`}
+        <ul className="navbar-nav mr-auto">
+          <li
+            className={`nav-item ${
+              location.pathname === "/addnotes" ? "active" : ""
+            } `}
           >
-            <input
-              className="form-check-input"
-              onClick={props.toggleMode}
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-            ></input>
-            <label
-              className="form-check-label"
-              htmlFor="flexSwitchCheckDefault"
+            <Link
+              className={` nav-link text-${props.textcolor}`}
+              to="/addNotes"
             >
-              Enable Dark Mode
-            </label>
-          </div>
+              AddNotes{" "}
+            </Link>
+          </li>
+        </ul>
 
-          {!localStorage.getItem("token") ? (
-            <form className="form-inline my-2 my-lg-0">
-              <Link
-                type="button"
-                className={`btn btn-outline-primary text-${props.textcolor} mx-2`}
-                to="#"
-                onClick={handleLoginModalShow}
-              >
-                LogIn
-              </Link>
-              <Link
-                type="button"
-                className={`btn btn-outline-primary text-${props.textcolor}`}
-                to="#"
-                onClick={handleSignupModalShow}
-              >
-                SignUp
-              </Link>
-            </form>
-          ) : (
-            <div className="dropdown">
-              <div onClick={handleLogout}>Logout</div>
-            </div>
-          )}
+        <div
+          className={`form-check form-switch text-${
+            props.mode === `light` ? `dark` : `light`
+          }`}
+        >
+          <input
+            className="form-check-input"
+            onClick={props.toggleMode}
+            type="checkbox"
+            role="switch"
+            id="flexSwitchCheckDefault"
+          ></input>
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+            Enable Dark Mode
+          </label>
         </div>
+
+        {!localStorage.getItem("token") ? (
+          <form className="form-inline my-2 my-lg-0">
+            <Link
+              type="button"
+              className={`btn btn-outline-primary text-${props.textcolor} mx-2`}
+              to="#"
+              onClick={handleLoginModalShow}
+            >
+              LogIn
+            </Link>
+            <Link
+              type="button"
+              className={`btn btn-outline-primary text-${props.textcolor}`}
+              to="#"
+              onClick={handleSignupModalShow}
+            >
+              SignUp
+            </Link>
+          </form>
+        ) : (
+          <div className="dropdown">
+            <div onClick={handleLogout}>Logout</div>
+          </div>
+        )}
       </nav>
     </>
   );
